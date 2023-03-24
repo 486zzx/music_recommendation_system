9d56cdf4992b0553345271dd10109d680c2745e6
zzx <345857107@qq.com>
2023-03-24 22:36:35 +0800

    init project

---
+++ src/main/java/com/zzx/zzx_music_recommendation_system/ZzxMusicRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zzx.zzx_music_recommendation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzxMusicRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzxMusicRecommendationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzx/zzx_music_recommendation_system/ZzxMusicRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.zzx_music_recommendation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzxMusicRecommendationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
